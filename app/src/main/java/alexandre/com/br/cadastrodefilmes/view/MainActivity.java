9c84adc6094209d823418b1ae90bc140b362c9bc
Projetos 1 <projetos1@companytec.com.br>
2017-08-23 15:42:19 -0300

    Initial commit

---
+++ app/src/main/java/alexandre/com/br/cadastrodefilmes/view/MainActivity.java
@@ -0,0 +1,15 @@
+package alexandre.com.br.cadastrodefilmes.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexandre.com.br.cadastrodefilmes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
